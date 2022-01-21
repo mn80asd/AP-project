7e4c742a573b5e71ef9d35c260ee64a6a83edf11
arsalanjabbari <arsalanjabbari2002@gmail.com>
2022-01-21 22:33:43 +0330

    add arsalan

--- src/main.java
+++ src/main.java
@@ -4,6 +4,6 @@ public class main {
         System.out.println("hello bad project");
         System.out.println("hello amir");
         System.out.println("booogh");
-
+        System.out.println("ARSALAN");
     }
 }
