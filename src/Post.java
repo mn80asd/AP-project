f1113e61a351500f36cd818e8fcc63a06ee2e90c
mona <mn80asd@gmail.com>
2022-01-22 10:09:39 +0330

    add post mona

--- src/Post.java
+++ src/Post.java
@@ -1,2 +1,42 @@
-public class Post {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class post {
+    private static int helpIdPost=0;
+    private final String name;
+    private final String description;
+    private int likeNumber;
+    private HashMap<String,String> comments = new HashMap<>();
+    private int postId;
+
+    public post(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.postId= helpIdPost++;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+    public void addLikeToPost(){
+        likeNumber+=1;
+    }
+    public void addCommentToPost(post p, String body,String personName){
+        comments.put(personName,body);
+    }
+
+    @Override
+    public String toString() {
+        return "Post {" +
+                "post id= "+ postId+
+                ", name= " + name +
+                ", description= " + description +
+                ", like number= "+ likeNumber +
+                ", comments= "+ comments+
+                '}';
+    }
 }
