f2a91d9110b86006563ba6bb29d7749a51dbcbb1
arsalanjabbari <arsalanjabbari2002@gmail.com>
2022-01-23 11:14:06 +0330

    test arsalan's git VCS

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class main {
-
+    public int test = 1;
     HashMap<String,Integer> UserPassMap = new HashMap<>();
     HashMap<String,Integer> NamePassGroupMap = new HashMap<>();
     static Person currentUser;
