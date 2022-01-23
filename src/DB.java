078b38b0bc0d80affc306a01649cf929ff3738ff
3mirebrahimi <amirkhamoosh0011@gmail.com>
2022-01-23 19:01:28 +0330

    DB second test

--- src/DB.java
+++ src/DB.java
@@ -25,16 +25,15 @@ public class DB {
     }
     public static Connection getConnection() throws Exception{}
         try{
-    String driver ="com.mysql.jdbc.Driver";
-    String url = "jdbc:mysql://24.196.52.166:3306/testab";
-    String username = "hey";
-    String password = "mypass";
-    Class.forName(driver);
-
-    Connection conn = DriverManager.getConnection(url, username, password);
-    System.out.println("Connected");
+            String driver ="com.mysql.jdbc.Driver";
+            String url = "jdbc:mysql://24.196.52.166:3306/testab";
+            String username = "hey";
+            String password = "mypass";
+            Class.forName(driver);
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected");
             return conn;
-} catch(Exception e){System.out.println(e);}
+        } catch(Exception e){System.out.println(e);}
 
 
         return null;
