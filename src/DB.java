bb79289aa40962cdff18b65b76b2f622b5c2b73e
3mirebrahimi <amirkhamoosh0011@gmail.com>
2022-01-24 11:52:33 +0330

    DB insert and select test

--- src/DB.java
+++ src/DB.java
@@ -1,17 +1,51 @@
 //package database;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.*;
 
 public class DB {
 
     public DB() throws SQLException, ClassNotFoundException {
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         // TODO Auto-generated method stub
         createTable();
+        post();
+        get();
+    }
+
+    public static ArrayList‹String> get() throws Exception{
+        try {
+            Connection con = getConnection();
+            PreparedStatement statement = con.prepareStatement("SELECT first,last FROM iamalive");
+
+            ResultSet result = statement.executeQuery();
+
+            ArrayList‹String›array = new ArrayList‹String›();
+            while (result.next()) {
+                System.out.print(result.getString("first"));
+                System.out.print(" ");
+                System.out.println(result.getString("last"));
+
+                array.add(result.getString("last"));
+            }
+            System.out.println("All records have been selected!");
+            return array;
+        } catch (Exception e) { System.out.println(e); }
+        return null;
+    }
+    public static void post() throws Exception {
+        final String var1 = "arsalan";
+        final String var2 = "mona";
+        try {
+            Connection con = getConnection();
+            PreparedStatement posted = con.prepareStatement("INSERT INTO tablename(first, last) VALUES ('" + var1 + "', '" + var2 + "')");
+            posted.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("Insert Completed.");
+        }
     }
     public static void createTable() throws Exception{
         try{
@@ -23,6 +57,7 @@ public class DB {
             System.out.println("Function Complete.");
         };
     }
+
     public static Connection getConnection() throws Exception{
         try{
             String driver ="com.mysql.jdbc.Driver";
